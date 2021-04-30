1cc5dc29b9cb688d322debdd60e42f7184b5703f
Cptn Reef <sharif.staffordbz@gmail.com>
2021-04-30 10:22:09 -0400

    flask_login

--- grocery_app/__init__.py
+++ grocery_app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from grocery_app.config import Config
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
@@ -9,9 +11,17 @@ app.secret_key = os.urandom(24)
 
 db = SQLAlchemy(app)
 
+bcrypt = Bcrypt(app)
+
 from grocery_app.routes import main
 
 app.register_blueprint(main)
+
+login_manager.login_view = "auth.login"
+
+@login_manager.user_loader
+def load_user(id):
+    pass
 
 with app.app_context():
     db.create_all()
