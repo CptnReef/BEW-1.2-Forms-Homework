25243e48e647a3e6519fa776e9567a4d54ca30f9
Cptn Reef <sharif.staffordbz@gmail.com>
2021-04-30 10:23:52 -0400

    login fixed

--- grocery_app/__init__.py
+++ grocery_app/__init__.py
@@ -17,6 +17,8 @@ from grocery_app.routes import main
 
 app.register_blueprint(main)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
 login_manager.login_view = "auth.login"
 
 @login_manager.user_loader
